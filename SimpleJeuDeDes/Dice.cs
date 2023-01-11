80e9195dff0c245f048e981a52cf28293479c96e
SylvainTorrenti <torrsyl@gmail.com>
2023-01-11 10:58:33 +0100

    Ajoutez des fichiers projet.

---
+++ SimpleJeuDeDes/Dice.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleJeuDeDes
+{
+    internal class Dice
+    {
+        #region Attribute
+        private int value;
+        private int number; 
+        #endregion
+        #region Get value
+        public int GetValue()
+        {
+            return value;
+        }
+
+        #endregion
+        #region Get & Set number
+        public int GetNumber()
+        {
+            return value;
+        }
+        public void SetNumber(int number)
+        {
+            this.number = number;
+        }
+        #endregion
+        #region Method
+        /// <summary>
+        /// Method to display Dice
+        /// </summary>
+        public void Print()
+        {
+            Console.WriteLine($"Valeur : {value}");
+        }
+        /// <summary>
+        /// Method to launch Dice
+        /// </summary>
+        public void Launch()
+        {
+            Random valueRandom = new Random();
+            this.value = valueRandom.Next(1, 6);
+        } 
+        #endregion
+    }
+
+}
---
+++ SimpleJeuDeDes/Program.cs
@@ -0,0 +1,18 @@
+﻿using SimpleJeuDeDes;
+#region Create Dice
+Dice De1 = new Dice();
+Dice De2 = new Dice();
+#endregion
+#region Shot
+int Shot = 0;
+do
+{
+    De1.Launch();
+    De2.Launch();
+    Shot++;
+} while (!(De1.GetValue() + De2.GetValue() == 6));
+Console.WriteLine($"Il il aura fallu {Shot} coup! "); 
+#endregion
+
+
+
